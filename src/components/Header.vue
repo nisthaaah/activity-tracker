72b976e3e9f8ca2c9e69f118367de17389ad5ba1
Nistha Sharma <122206309+nisthaaah@users.noreply.github.com>
2024-01-27 20:31:06 +0530

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <TaskButton @btn-click="$emit('toggle-add-task')" text = "Add Task" color="green" />
+    </header>
+
+</template>
+
+<script>
+import TaskButton from './Button.vue';
+export default{
+    name: 'TaskHeader',
+    props:{
+        title : String
+    },
+    components: {
+        TaskButton,
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
